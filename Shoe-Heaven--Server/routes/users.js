2d10cf876e9e558fe7479050cbe8abc2b4cad1a5
salinda-yasas-99 <salindasym@gmail.com>
2024-05-15 00:46:31 +0530

    ading changes

--- Shoe-Heaven--Server/routes/users.js
+++ Shoe-Heaven--Server/routes/users.js
@@ -1,17 +1,21 @@
-//import express from "express";
-//import userController from "../controllers/users.js";
-//import { addNewUser, getUsers, deleteUser } from "../controllers/users.js";
 const express = require("express");
 const userController = require("../controllers/users");
+const authController = require("../controllers/auth");
 
 const router = express.Router();
 const path = require("path");
-//const userController = require("../controllers/users.js");
 
-router.get("/getAllUsers", userController.getAllUsers);
+router.get(
+  "/getAllUsers",
+  authController.accessAuthorizeAdmin,
+  userController.getAllUsers
+);
 router.post("/register/user", userController.addNewUser);
 router.post("/register/admin", userController.addNewAdmin);
-router.delete("/delete/:Id", userController.deleteUser);
+router.delete(
+  "/delete/:Id",
+  authController.accessAuthorizeAdmin,
+  userController.deleteUser
+);
 
 module.exports = router;
-//export default router;
