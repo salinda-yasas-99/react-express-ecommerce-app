20bf267d230597148eb927c3af6226849caf981e
Udayanga-Deeshan <Udayangadilshan1789@gmail.com>
2024-07-11 21:44:52 +0530

    added Hero section

--- Shoe-Heaven--Client-App/src/Pages/Shop.jsx
+++ Shoe-Heaven--Client-App/src/Pages/Shop.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import CustomerSupport from '../Components/CustomerSupport/CustomerSupport'
+import Hero from '../Components/Hero/Hero'
 
 const Shop = () => {
   return (
     <div>
-       <h1>New collections</h1>
+       <Hero/>
        <CustomerSupport/>
     </div>
   )
