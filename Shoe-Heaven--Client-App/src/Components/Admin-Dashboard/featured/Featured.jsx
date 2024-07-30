1c8c075ee818db66cbb22c2269eaa274ef130b30
salinda-yasas-99 <salindasym@gmail.com>
2024-07-30 23:13:51 +0530

    adding count

--- Shoe-Heaven--Client-App/src/Components/Admin-Dashboard/featured/Featured.jsx
+++ Shoe-Heaven--Client-App/src/Components/Admin-Dashboard/featured/Featured.jsx
@@ -5,23 +5,22 @@ import "react-circular-progressbar/dist/styles.css";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
 
-const Featured = () => {
+const Featured = ({ count }) => {
   return (
     <div className="featured">
       <div className="top">
-        <h1 className="title">Total Revenue</h1>
+        <h1 className="title">Total This Month Orders</h1>
         <MoreVertIcon fontSize="small" />
       </div>
       <div className="bottom">
         <div className="featuredChart">
-          <CircularProgressbar value={70} text={"70%"} strokeWidth={5} />
+          {/* <CircularProgressbar value={count} text={count} strokeWidth={5} /> */}
+          {"This is total this month orders"}
+          <span style={{ color: "red" }}>{count}</span>
         </div>
-        <p className="bottomTitle">Total sales made in this month</p>
-        <p className="amount">Rs:27000</p>
-        <p className="desc">
-           Last payments may not be included.
-        </p>
-       
+        {/* <p className="bottomTitle">Total sales made in this month</p> */}
+        {/* <p className="amount">Rs:27000</p> */}
+        {/* <p className="desc">Last payments may not be included.</p> */}
       </div>
     </div>
   );
